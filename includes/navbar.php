da3d4b35bb5eec0770c630cc67d862c7c3d4a8ed
ryliezachary <losganoryan0808@gmail.com>
2019-08-16 10:48:08 -0400

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 				<nav class="navbar fixed-top navbar-expand-lg navbar-light">
-				  <a class="navbar-brand" href="#"><img src="./images/favicon.png" alt="LOGO"></a>
+				  <a class="navbar-brand" href="#"><img src="./images/favicon.png" alt="LOGO" height="35" width="43"></a>
 				   <button class="navbar-toggler text-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				    <span class="navbar-toggler-icon"></span>
 				  </button>
